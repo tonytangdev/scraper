5b544621519e91b83e8831af9d38e491d65dd9a9
tonytangdev <tony.tang.developer@gmail.com>
2025-03-04 14:00:33 +0100

    feat: add Article entity with properties and constructor

---
+++ src/article/core/entities/article.entity.ts
@@ -0,0 +1,44 @@
+import { randomUUID } from 'node:crypto';
+import { Url } from '../../../shared/value-objects/url';
+
+type ArticleProps = {
+  id?: string;
+  title: string;
+  url: string;
+  source: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+  publicationDate: Date;
+};
+
+export class Article {
+  private id: string;
+  private title: string;
+  private url: Url;
+  private source: string;
+  private createdAt: Date;
+  private updatedAt: Date;
+  private deletedAt: Date | null;
+  private publicationDate: Date;
+
+  constructor({
+    id = randomUUID(),
+    title,
+    url,
+    source,
+    createdAt = new Date(),
+    updatedAt = new Date(),
+    deletedAt = null,
+    publicationDate,
+  }: ArticleProps) {
+    this.id = id;
+    this.title = title;
+    this.url = new Url(url);
+    this.source = source;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
+    this.publicationDate = publicationDate;
+  }
+}
