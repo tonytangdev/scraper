41710086ce7250fd377b72c9d27d21f11513789f
tonytangdev <tony.tang.developer@gmail.com>
2025-03-04 12:05:53 +0100

    feat: add Job entity and Url value object for scraping jobs

---
+++ src/scraping/core/entities/job.entity.ts
@@ -0,0 +1,63 @@
+import { randomUUID } from 'node:crypto';
+import { Url } from '../value-objects/url';
+
+export enum JobStatus {
+  IDLE = 'IDLE',
+  IN_PROGRESS = 'IN_PROGRESS',
+  SUCCESS = 'SUCCESS',
+  FAIL = 'FAIL',
+}
+
+type JobProps = {
+  url: string;
+  id?: string;
+  status?: JobStatus;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date | null;
+};
+
+export class Job {
+  private id: string;
+  private url: Url;
+  private status: JobStatus;
+  private createdAt: Date;
+  private updatedAt: Date;
+  private deletedAt: Date | null;
+
+  constructor({
+    url,
+    id = randomUUID(),
+    status = JobStatus.IDLE,
+    createdAt = new Date(),
+    updatedAt = new Date(),
+    deletedAt = null,
+  }: JobProps) {
+    this.id = id;
+    this.url = new Url(url);
+    this.status = status;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.deletedAt = deletedAt;
+  }
+
+  getUrl(): string {
+    return this.url.getUrl();
+  }
+
+  getStatus(): JobStatus {
+    return this.status;
+  }
+
+  markAsInProgress(): void {
+    this.status = JobStatus.IN_PROGRESS;
+  }
+
+  markAsSuccess(): void {
+    this.status = JobStatus.SUCCESS;
+  }
+
+  markAsFail(): void {
+    this.status = JobStatus.FAIL;
+  }
+}
---
+++ src/scraping/core/value-objects/url.ts
@@ -0,0 +1,26 @@
+export class Url {
+  private readonly url: string;
+
+  constructor(url: string) {
+    const isValid = this.validateUrl(url);
+    if (!isValid) {
+      throw new Error('Invalid URL');
+    }
+
+    this.url = url;
+  }
+
+  validateUrl(url: string): boolean {
+    try {
+      new URL(url);
+      return true;
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  }
+
+  getUrl(): string {
+    return this.url;
+  }
+}
