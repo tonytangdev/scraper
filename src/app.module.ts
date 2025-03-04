1db417795bafeead94f7fe4d92dafac1b13b89a4
tonytangdev <tony.tang.developer@gmail.com>
2025-03-04 14:00:40 +0100

    feat: add ArticleModule to the application module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ScrapingModule } from './scraping/scraping.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ScrapingModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScrapingModule,
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
