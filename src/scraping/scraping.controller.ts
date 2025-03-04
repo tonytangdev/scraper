a82fb04941b367eb3b71cceb0f801c7ac2b02eef
tonytangdev <tony.tang.developer@gmail.com>
2025-03-04 14:22:12 +0100

    feat: call scraping service when POST /scrapings is called

--- src/scraping/scraping.controller.ts
+++ src/scraping/scraping.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { CreateScrapingJobDTO } from './dtos/create-scraping-job.dto';
+import { ScrapingService } from './scraping.service';
 
 @Controller('scrapings')
 export class ScrapingController {
-  constructor() {}
+  constructor(private readonly scrapingService: ScrapingService) {}
 
   @Post()
-  createScrapingJob(@Res() res: Response) {
+  createScrapingJob(@Body() dto: CreateScrapingJobDTO, @Res() res: Response) {
+    // Don't need to wait for the job to finish
+    void this.scrapingService.createScrapingJob(dto);
     res.status(HttpStatus.CREATED).send();
   }
 }
